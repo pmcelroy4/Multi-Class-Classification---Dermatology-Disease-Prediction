e9b61942fa6a587e25f35c350ab31c3f953b0d4f
pmcelroy4 <patrickmcelroy4.pm@gmail.com>
2026-01-08 17:24:35 -0500

    Cleaning up repo

--- models/train/logistic_regression.py
+++
@@ -1,8 +0,0 @@
-# Import necessary libraries
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-
